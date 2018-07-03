21b4430abbca3ef7972541d101c49eab18fcb17f
m@rtlin2cz <martlin2cz@gmail.com>
2018-07-03 19:53:39 +0200

    Just added files to previous commit
     - yea

---
+++ jmop/src/test/java/cz/martlin/jmop/core/sources/download/ConverterTest.java
@@ -0,0 +1,57 @@
+package cz.martlin.jmop.core.sources.download;
+
+import java.io.File;
+import java.io.IOException;
+
+import cz.martlin.jmop.core.misc.ProgressListener;
+import cz.martlin.jmop.core.sources.AbstractRemoteSource;
+import cz.martlin.jmop.core.sources.SourceKind;
+import cz.martlin.jmop.core.sources.Sources;
+import cz.martlin.jmop.core.sources.local.AbstractFileSystemAccessor;
+import cz.martlin.jmop.core.sources.local.BaseFilesNamer;
+import cz.martlin.jmop.core.sources.local.BaseLocalSource;
+import cz.martlin.jmop.core.sources.local.DefaultFileSystemAccessor;
+import cz.martlin.jmop.core.sources.local.DefaultFilesNamer;
+import cz.martlin.jmop.core.sources.local.DefaultLocalSource;
+import cz.martlin.jmop.core.sources.local.TrackFileFormat;
+import cz.martlin.jmop.core.sources.remotes.YoutubeSource;
+import cz.martlin.jmop.core.tracks.Bundle;
+import cz.martlin.jmop.core.tracks.Track;
+import cz.martlin.jmop.core.tracks.TrackIdentifier;
+
+public class ConverterTest {
+	public static void main(String[] args) throws IOException {
+		final String id = "3V7EugoweM4";
+		final String title = "Something";
+		final String description = "Something interresting";
+		final String bundleName = "testing-tracks";
+		final File rootDir = File.createTempFile("xxx", "xxx").getParentFile(); // hehe
+		final SourceKind source = SourceKind.YOUTUBE;
+
+		AbstractRemoteSource remote = new YoutubeSource();
+
+		BaseFilesNamer namer = new DefaultFilesNamer(rootDir);
+		AbstractFileSystemAccessor fileSystem = new DefaultFileSystemAccessor(namer);
+		Bundle bundle = new Bundle(source, bundleName);
+
+		BaseLocalSource local = new DefaultLocalSource(fileSystem, bundle);
+		Sources sources = new Sources(local, remote);
+		
+		TrackFileFormat inputFormat = TrackFileFormat.OPUS;
+		TrackFileFormat outputFormat = TrackFileFormat.MP3;
+		
+		ProgressListener listener = new SimpleLoggingListener(System.out);
+		BaseSourceConverter converter = new FFMPEGConverter(sources, inputFormat, outputFormat, listener);
+
+		TrackIdentifier identifier = new TrackIdentifier(source, id);
+		Track track = new Track(identifier, title, description);
+
+		try {
+			boolean success = converter.convert(track);
+			System.err.println("Success? " + success);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
---
+++ jmop/src/test/java/cz/martlin/jmop/core/sources/download/DownloaderTaskTest.java
@@ -0,0 +1,69 @@
+package cz.martlin.jmop.core.sources.download;
+
+import java.io.File;
+
+import cz.martlin.jmop.core.misc.ProgressListener;
+import cz.martlin.jmop.core.sources.AbstractRemoteSource;
+import cz.martlin.jmop.core.sources.SourceKind;
+import cz.martlin.jmop.core.sources.Sources;
+import cz.martlin.jmop.core.sources.local.AbstractFileSystemAccessor;
+import cz.martlin.jmop.core.sources.local.BaseFilesNamer;
+import cz.martlin.jmop.core.sources.local.BaseLocalSource;
+import cz.martlin.jmop.core.sources.local.DefaultFileSystemAccessor;
+import cz.martlin.jmop.core.sources.local.DefaultFilesNamer;
+import cz.martlin.jmop.core.sources.local.DefaultLocalSource;
+import cz.martlin.jmop.core.sources.local.TrackFileFormat;
+import cz.martlin.jmop.core.sources.remotes.YoutubeSource;
+import cz.martlin.jmop.core.tracks.Bundle;
+import cz.martlin.jmop.core.tracks.Track;
+import cz.martlin.jmop.core.tracks.TrackIdentifier;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DownloaderTaskTest extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		final String id = "3V7EugoweM4";
+		final String title = "Something";
+		final String description = "Something interresting";
+		final String bundleName = "another-testing-tracks";
+		final File rootDir = File.createTempFile("xxx", "xxx").getParentFile(); // hehe
+		final SourceKind source = SourceKind.YOUTUBE;
+
+		AbstractRemoteSource remote = new YoutubeSource();
+
+		BaseFilesNamer namer = new DefaultFilesNamer(rootDir);
+		AbstractFileSystemAccessor fileSystem = new DefaultFileSystemAccessor(namer);
+		Bundle bundle = new Bundle(source, bundleName);
+
+		BaseLocalSource local = new DefaultLocalSource(fileSystem, bundle);
+		Sources sources = new Sources(local, remote);
+		ProgressListener listener = new SimpleLoggingListener(System.out);
+		BaseSourceDownloader downloader = new YoutubeDlDownloader(sources, listener);
+
+		TrackIdentifier identifier = new TrackIdentifier(source, id);
+		Track track = new Track(identifier, title, description);
+
+		TrackFileFormat inputFormat = TrackFileFormat.OPUS;
+		TrackFileFormat outputFormat = TrackFileFormat.MP3;
+
+		BaseSourceConverter converter = new FFMPEGConverter(sources, inputFormat, outputFormat, listener);
+
+		DownloaderTask task = new DownloaderTask(downloader, converter, track);
+		
+		task.messageProperty().addListener((observable, oldVal, newVal) -> {
+			System.out.println("# " + newVal);
+		});
+		task.setOnSucceeded((e) -> {
+			System.out.print("Done!");
+		});
+
+		task.run();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+}
