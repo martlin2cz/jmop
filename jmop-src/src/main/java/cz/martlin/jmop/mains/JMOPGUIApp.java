d6cae38923835266e0eee90967b38b05d46bda2a
martlin2cz2 <martlin@seznam.cz>
2019-07-13 00:09:54 +0200

    Created the 1.1 release
     - updated version in pom and gui
     - built and zipped

--- jmop-src/src/main/java/cz/martlin/jmop/mains/JMOPGUIApp.java
+++ jmop-src/src/main/java/cz/martlin/jmop/mains/JMOPGUIApp.java
@@ -15,7 +15,7 @@ import cz.martlin.jmop.gui.local.Msg;
 
 public class JMOPGUIApp {
 	private static final Logger LOG = LoggerFactory.getLogger(JMOPGUIApp.class);
-	public static final String VERSION = "1.0"; //$NON-NLS-1$
+	public static final String VERSION = "1.1"; //$NON-NLS-1$
 
 	private static JMOPPlayer jmop;
 
@@ -98,6 +98,7 @@ public class JMOPGUIApp {
 	public static JMOPPlayer getJmop() {
 		//FIXME java 11 mvn hack
 		if (jmop == null) {
+			LOG.warn("The JMOPPlayer cannot be obtained, using default");
 			try {
 				jmop = new DefaultJMOPPlayerBuilder().create(new CommandlineData());
 			} catch (Exception e) {
