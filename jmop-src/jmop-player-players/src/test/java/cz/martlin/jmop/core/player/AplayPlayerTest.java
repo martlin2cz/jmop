cab621921a0cd49562a36a15ab68d920b967dd77
martlin2cz2 <martlin@seznam.cz>
2020-12-21 23:28:06 +0100

    Disabled the aplay test on windows
     - yup

--- jmop-src/jmop-player-players/src/test/java/cz/martlin/jmop/core/player/AplayPlayerTest.java
+++ jmop-src/jmop-player-players/src/test/java/cz/martlin/jmop/core/player/AplayPlayerTest.java
@@ -1,5 +1,8 @@
 package cz.martlin.jmop.core.player;
 
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
 import cz.martlin.jmop.common.musicbase.TracksSource;
 import cz.martlin.jmop.common.utils.TestingTracksSource;
 import cz.martlin.jmop.core.misc.BaseErrorReporter;
@@ -7,6 +10,7 @@ import cz.martlin.jmop.core.misc.SimpleErrorReporter;
 import cz.martlin.jmop.core.player.base.player.BasePlayer;
 import cz.martlin.jmop.core.sources.local.TrackFileFormat;
 
+@DisabledOnOs(value = OS.WINDOWS)
 public class AplayPlayerTest extends AbstractPlayerTest {
 
 	public AplayPlayerTest() {
