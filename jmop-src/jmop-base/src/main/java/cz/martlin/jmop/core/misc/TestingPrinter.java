4ac86ca6dfe6bd5fee32d1e7eba87b15dc027b4d
martlin2cz2 <martlin@seznam.cz>
2021-09-18 15:04:49 +0200

    Merge branch 'jmop_v2_separated_to_modules' into jmop_v2_DEV
     - finally managed to separate the jmop project into the modules
     - created set of modules, namelly:
     - jmop-X-Y, where:
      - X can be either player, sourcery or the common
      - Y can be X-specific (storages, players, playlisters, cli, gui, app, remotes, ...)

---
+++ jmop-src/jmop-base/src/main/java/cz/martlin/jmop/core/misc/TestingPrinter.java
@@ -0,0 +1,32 @@
+package cz.martlin.jmop.core.misc;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import cz.martlin.jmop.core.data.Track;
+
+public class TestingPrinter {
+
+	public static String print(Track track) {
+		return "" //
+				+ " ID:    " + track.getIdentifier() + "\n"//
+				+ " TITLE: " + track.getTitle() + "\n"//
+				+ " TIME:  " + DurationUtilities.toHumanString(track.getDuration()) + "\n"//
+				+ " DESC:  " + shorten(track.getDescription()) + "\n"//
+				+ "\n"; //
+	}
+
+	public static String print(List<Track> tracks) {
+		return tracks.stream().map((t) -> print(t)).collect(Collectors.joining("\n"));
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	private static String shorten(String text) {
+		if (text.length() > 80) {
+			return text.substring(0, 80) + "(...)";
+		} else {
+			return text;
+		}
+	}
+}
